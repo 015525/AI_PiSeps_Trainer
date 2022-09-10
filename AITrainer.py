da86136b2629e05db0f88bef0687c2800e6a8443
Abdelrhman Yousry <abdo.ahmed022@gmail.com>
2022-09-10 19:42:12 +0200

    added hand turns feature

--- AITrainer.py
+++ AITrainer.py
@@ -17,28 +17,35 @@ def check_weight(img, current_duration_for_curl, counter, wrong_position):
     #if (len(self.lm_list) > 16):
     x, y = 950, 320
     if wrong_position :
-        cv2.putText(img, f'  Make Your ', (x + 50, y - 90),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        cv2.putText(img, f'Elbow in Line', (x + 50, y - 65),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        cv2.putText(img, f'  With Your ', (x + 50, y - 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-        cv2.putText(img, f'  shoulder', (x + 50, y - 15),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        cv2.putText(img, f'   Make Your ', (x + 50, y - 90),
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(img, f' Elbow in Line', (x + 50, y - 65),
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(img, f'   With Your ', (x + 50, y - 40),
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(img, f'   shoulder!', (x + 50, y - 15),
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
     elif current_duration_for_curl > 3 and counter > 6:
-        cv2.putText(img, f'Good job just', (x + 50, y - 70), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+        cv2.putText(img, f'Good job just', (x + 50, y - 70), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 2)
         cv2.putText(img, f' remaining {10 - counter}', (x + 50, y - 25),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 2)
     elif current_duration_for_curl > 3 and counter <= 6:
         cv2.putText(img, f'  We are just', (x + 50, y - 90),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
         cv2.putText(img, f'   in curl {counter}', (x + 50, y - 65),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
         cv2.putText(img, f' May be this is', (x + 50, y - 40),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
         cv2.putText(img, f'a heavy weight', (x + 50, y - 15),
-                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255), 2)
 
+def tell_hand_order(img,hand):
+    #if (len(self.lm_list) > 16):
+    x, y = 950, 320
+    cv2.putText(img, f'  {hand} hand ', (x + 50, y - 90),
+                cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 2)
+    cv2.putText(img, f'     Turn ', (x + 50, y - 45),
+                cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 2)
 
 
 def aiTrainer() :
@@ -51,7 +58,7 @@ def aiTrainer() :
     pose = pm.poseDetector(min_detection_confidence=0.8)
     piStat = ps.PiSeps()
     group = 1
-    hand_order = "right"
+    hand_order = "Right"
 
     timer = 11
     counter = 0
@@ -67,24 +74,48 @@ def aiTrainer() :
             pTime=cTime
             first_time_enter=False
 
-        if hand == "Right" :
+        if hand == "Right":
             verified_hand = "Right"
-        elif hand == "Left" :
+        elif hand == "Left":
             verified_hand = "Left"
 
-        if verified_hand == "Left" :
+        if verified_hand == "Left":
             hand_cof= -1
-        elif verified_hand == "Right" :
+        elif verified_hand == "Right":
             hand_cof= 1
 
         coach_img = cv2.imread("Images/coach.jpg")
         img = np.concatenate((img, coach_img), axis=1)
 
-        if timer == 0:
+        '''
+        if hand_order!=hand and (timer==0 or timer==-1):
+            timer_shown = 0
+            timer = -1
+        elif hand_order==hand and timer==-1:
+            timer=0
+            timer_shown=timer
+        else:
+            timer_shown=timer
+            
+        if :
+            hand_order_verified=True
+        else:
+            hand_order_verified=False
+
+        print(f'hand_order_verified is {hand_order_verified}')
+        '''
+
+
+        if not timer and hand_order==verified_hand:
             wrong_pos = piStat.wrongElbowPosition(img, verified_hand)
             counter, current_duration_for_curl = piStat.count_curl_and_time(img, hand_cof, verified_hand)
-            print(f'counter in if timer is {counter}')
+            #print(f'counter in if timer is {counter} and hand_order is {hand_order}')
             check_weight(img, current_duration_for_curl, counter, wrong_pos)
+        elif hand_order!=verified_hand :
+            tell_hand_order(img, hand_order)
+        elif 0<timer<=3:
+            cv2.putText(img, f'  BE READY ', (1000, 230),
+                        cv2.FONT_HERSHEY_DUPLEX, 1, (255, 0, 0), 2)
 
 
         if cTime - pTime >= 1 and timer > 0:
@@ -92,18 +123,18 @@ def aiTrainer() :
             pTime = cTime
 
 
-        cv2.putText(img, f'Count: {counter}', (10, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.putText(img, f'Posture Position: {right_pos}', (10, 90), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.putText(img, f'Hand: {verified_hand}', (10, 130), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.putText(img, f'Group: {group}', (700, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-        cv2.putText(img, f'Timer: {timer}', (700, 90), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.putText(img, f'Count: {counter}', (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (42, 42, 162), 2)
+        cv2.putText(img, f'Posture Position: {right_pos}', (10, 90), cv2.FONT_HERSHEY_DUPLEX, 1, (42, 42, 162), 2)
+        cv2.putText(img, f'Hand: {verified_hand}', (10, 130), cv2.FONT_HERSHEY_DUPLEX, 1, (42, 42, 162), 2)
+        cv2.putText(img, f'Group: {group}', (700, 50), cv2.FONT_HERSHEY_DUPLEX, 1, (42, 42, 162), 2)
+        cv2.putText(img, f'Timer: {timer}', (700, 90), cv2.FONT_HERSHEY_DUPLEX, 1, (42, 42, 162), 2)
 
         if counter == 10:
             print(f'in if and counter is {counter}')
             if first_enter:
                 group += 1
                 #todo change hand every group and set a timer
-                hand_order = "left" if hand_order == "right" else "right"
+                hand_order = "Left" if hand_order == "Right" else "Right"
                 counter = 0
                 timer = 15
                 first_enter = False
@@ -112,6 +143,10 @@ def aiTrainer() :
             first_enter = True
 
         if group == 5 :
+            while cTime-pTime<=2.5:
+                cv2.putText(img, f' Good Job!! ', (1000, 230),
+                            cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                cTime=time.time()
             return True
 
         cv2.imshow("AI Trainer", img)
--- choiceLoop.py
+++ choiceLoop.py
@@ -32,9 +32,9 @@ def choice() :
         img = generate_image(img, img_2, 167, 426)
         #img[5:5 + h1, 220: 220 + w1] = img_1
         #img[330:330 + h2, 330: 330 + w2] = img_2
-        cv2.putText(img, f'If ready then ', (200, 210), cv2.FONT_HERSHEY_PLAIN, 2,
+        cv2.putText(img, f'If ready then ', (200, 210), cv2.FONT_HERSHEY_DUPLEX, 1,
                     (255, 0, 0), 2)
-        cv2.rectangle(img, (220, 220), (460, 460), (162, 42, 42), 3)
+        cv2.rectangle(img, (200, 220), (460, 460), (42, 42, 162), 3)
 
         cTime = time.time()
         is_thumb_up, xb1, xb2, yb1 = choice.get_choice(img)
@@ -42,12 +42,12 @@ def choice() :
 
         #print(is_thumb_up, xb1, xb2, yb1)
         if is_thumb_up and xb1<470 and  xb2>180 and yb1>180:# and yb2<470 :
-            print("in first if ")
+            #print("in first if ")
             if first_enter_1 :
                 pTime = cTime
                 first_enter_1 = False
             if (cTime-pTime) >= 2 :
-                print("start exercise")
+                #print("start exercise")
                 return "start exercise"
         else :
             first_enter_1 = True
